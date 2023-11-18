045703c538bc07407c674dbd05e920698d26d452
Cyberdragon247 <151013386+Cyberdragon247@users.noreply.github.com>
2023-11-18 21:57:33 +0530

    Add files via upload

---
+++ start.py
@@ -0,0 +1,44 @@
+from telethon import events, Button
+from config import MK1, MK2, MK3, MK4, MK5, MK6, MK7, MK8, MK9, MK10
+from AltronX.modules.help import *
+import telethon
+
+PythonButton = [
+        [
+        Button.inline("☆ 𝐂σммαиԃ𝐒 ☆", data="help_back")
+        ],
+        [
+        Button.url("☆ 𝐂нαииє𝐋 ☆", "https://t.me/cyberdragon47"),
+        Button.url("☆ 𝐒υρρσя𝐓", "https://t.me/cyberdragonclub")
+        ],
+        [
+        Button.url("☆ 𝐑ꫀρ𝐎", "https://t.me/cyberdragonclub")
+        ]
+        ]
+
+
+@MK1.on(events.NewMessage(pattern="/start"))
+@MK2.on(events.NewMessage(pattern="/start"))
+@MK3.on(events.NewMessage(pattern="/start"))
+@MK4.on(events.NewMessage(pattern="/start"))
+@MK5.on(events.NewMessage(pattern="/start"))
+@MK6.on(events.NewMessage(pattern="/start"))
+@MK7.on(events.NewMessage(pattern="/start"))
+@MK7.on(events.NewMessage(pattern="/start"))
+@MK8.on(events.NewMessage(pattern="/start"))
+@MK9.on(events.NewMessage(pattern="/start"))
+@MK10.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+        AltBot = await event.client.get_me()
+        BotName = AltBot.first_name
+        BotId = AltBot.id
+        TEXT = f"**𝐇𝐞𝐲 [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝐈 𝐀𝐦 [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **✦ 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐃 𝐁𝐘 :~ [𝐂𝐘𝐁𝐄𝐑𝐃𝐑𝐀𝐆𝐎𝐍](https://t.me/Cyberdragon2477)**\n\n"
+        TEXT += f"» **𝐂𝐃 𝐒𝐏𝐀𝐌 𝐁𝐎𝐓 :** `3.2`\n"
+        TEXT += f"» **𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍:** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
+        await event.client.send_file(
+                event.chat_id,
+                "https://te.legra.ph/file/a1f6ca71c16eb940ffa36.jpg",
+                caption=TEXT, 
+                buttons=PythonButton)
